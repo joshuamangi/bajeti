1bbc2485f6c0c05568c1191dd1512cab3f141470
Joshua Mangi <joshuamangi@gmail.com>
2025-09-01 01:04:05 +0300

    Created working user schema and auth router

--- routers/auth.py
+++ routers/auth.py
@@ -1,22 +1,32 @@
-from datetime import datetime
+"""Auth Router"""
+from datetime import datetime, timedelta
 from typing import Optional
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from passlib.context import CryptContext
 
-from schema.user import UserOut
+from jose import JWTError, jwt
+from schema.user import UserCreate, UserOut, Token
 
 router = APIRouter(prefix="/api/auth")
 
+SECRET_KEY = "793d5117a7cf19f5ce87e7798ade10ddcbf883836aab149d0972bac514b73b44"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 # Password hashing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 mock_user = {
-    "id": 1,
-    "first_name": "Joshua",
-    "last_name": "Masumbuo",
-    "email": "joshuamangi@gmail.com",
-    "hashed_password": pwd_context.hash("password123"),
-    "created_at": datetime.utcnow()
+    "joshuamangi@gmail.com": {
+        "id": 1,
+        "first_name": "Joshua",
+        "last_name": "Masumbuo",
+        "email": "joshuamangi@gmail.com",
+        "hashed_password": pwd_context.hash("password123"),
+        "created_at": datetime.utcnow()
+    }
 }
 
 
@@ -24,7 +34,7 @@ def get_user(email: str) -> Optional[UserOut]:
     """Method used to return a user from email"""
     user = mock_user.get(email)
     if user:
-        return UserOut(*user)
+        return UserOut(**user)
     return None
 
 
@@ -43,13 +53,68 @@ def authenticate_user(email: str, plain_password: str) -> Optional[UserOut]:
     return user
 
 
-# Method for creating token
-# Maethod for getting current user from authenticated token
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    """Method for creating token"""
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-# Only authenticated user can perform these
 
-# Endpoint for login which returns token
-# Endpoint for getting token which returns User
+def get_current_user(token: str = Depends(oauth2_scheme)) -> UserCreate:
+    """Method for getting current user from authenticated token"""
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        user_id: int = payload.get("user_id")
+        if email is None or user_id is None:
+            raise credentials_exception
+    except JWTError as could_not_validate:
+        raise credentials_exception from could_not_validate
+
+    user = get_user(email)
+    if user is None or user.id != user_id:
+        raise credentials_exception
+
+    return user
+
+
+@router.post("/token", response_model=Token)
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    """
+        Endpoint for login which returns token
+        Request {"username: "user1@example.com", "password: "password123"}
+    """
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email, "user_id": user.id},
+        expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/users/me", response_model=UserCreate)
+async def read_users_me(current_user: UserCreate = Depends(get_current_user)):
+    """
+        Endpoint for returning current user using the
+        get_current_user depency and returns authenticated
+        user
+    """
+    return current_user
+
+
 # Endpoint for creating User in DB returns Created
 # Enpoint for editing user returns Edited User
 # Endpoint for removing user returns No content or message
--- schema/user.py
+++ schema/user.py
@@ -1,25 +1,31 @@
-# schema for User
-
+# Models the user
 from datetime import datetime
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
+
+
+class Token(BaseModel):
+    """Defines the Token structure"""
+    access_token: str
+    token_type: str
 
 
 class UserBase(BaseModel):
-    """defines the structure of the User that can be safely returned"""
+    """Defines the Base structure for creating a User"""
     first_name: str
-    email: EmailStr
-
-
-class UserCreate(UserBase):
-    """defines the structure for creating a user"""
+    last_name: str
+    email: str
     password: str
 
 
-class UserOut(UserBase):
-    """defines the structure for Useer data to be returned"""
+class UserCreate(BaseModel):
+    """Defines the structure for a User being Created and without hidden information"""
     id: int
+    first_name: str
+    last_name: str
+    email: str
     created_at: datetime
 
-    class Config:
-        """allows for the creation of sql alchemy objects"""
-        orm_mode = True
+
+class UserOut(UserCreate):
+    """Defines the structure for the User created in the database"""
+    hashed_password: str
