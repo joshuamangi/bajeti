6d30ed13ca446e89c79f3c01e6ed26832ac2c02b
Joshua Mangi <joshuamangi@gmail.com>
2025-09-23 02:08:13 +0300

    Added the bajeti.db in docker-compose

--- data/db/db.py
+++ data/db/db.py
@@ -1,8 +1,18 @@
 """Describes the database operations with sqlalchemy"""
+
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./bajeti.db"  # file-based SQLite
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
+# Make sure the folder exists
+os.makedirs(DATA_DIR, exist_ok=True)
+
+DB_PATH = os.path.join(DATA_DIR, "bajeti.db")
+
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
