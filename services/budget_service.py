8d6c674c4dfb981103dd2d76ef43c38810d3ed6c
Joshua Mangi <joshuamangi@gmail.com>
2025-12-14 00:45:06 +0300

    Added the backed db services and routes/routers for Budgets

---
+++ services/budget_service.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+from sqlalchemy.orm import Session
+
+from data.db.models.models import Budget
+from schema.budget import BudgetBase
+
+
+class BudgetService:
+    @staticmethod
+    def check_budget_exists(db: Session, user_id: int, budget: BudgetBase):
+        # query to add budget
+        existing_budget = db.query(Budget).filter(
+            Budget.name == budget.name,
+            Budget.user_id == user_id
+        ).first()
+
+        return existing_budget
+
+    @staticmethod
+    def save_new_budget(db: Session, user_id: int, budget: BudgetBase):
+        new_budget = Budget(
+            name=budget.name,
+            amount=budget.amount,
+            user_id=user_id
+        )
+        db.add(new_budget)
+        db.commit()
+        db.refresh(new_budget)
+        return new_budget
+
+    @staticmethod
+    def get_all_budgets(db: Session, user_id: int):
+        budgets = db.query(Budget).filter(
+            Budget.user_id == user_id
+        ).all()
+        return budgets
+
+    @staticmethod
+    def get_budget_by_id(db: Session, user_id: int, budget_id: int):
+        budget = db.query(Budget).filter(
+            Budget.user_id == user_id,
+            Budget.id == budget_id
+        ).first()
+        return budget
+
+    @staticmethod
+    def update_budget(db: Session, existing_budget: Budget, updated_budget: BudgetBase):
+        existing_budget.name = updated_budget.name
+        existing_budget.amount = updated_budget.amount
+        existing_budget.updated_at = datetime.utcnow()
+
+        db.commit()
+        db.refresh(existing_budget)
+        return existing_budget
+
+    @staticmethod
+    def remove_budget(db: Session, budget: Budget):
+        db.delete(budget)
+        db.commit()
